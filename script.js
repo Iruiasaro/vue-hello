d34aab341c813e61ea2b145fb1c07811e21d554c
Iruiasaro <brunettirosario@gmail.com>
2021-04-29 16:30:39 +0200

    message from Data JS

---
+++ script.js
@@ -0,0 +1,10 @@
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+const app = new Vue(
+    {
+        el: '#vueContainer',
+
+        data: {
+            message: 'Have a Nice Day!!!!',
+            title: 'titolo',
+        },
+    });
